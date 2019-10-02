4ae0cecd11c3e52efbb649093f89f07728cdee56
Elies AMARELO <elies.amarelo@greenwishes.fr>
2019-10-02 10:33:46 +0200

    [fix] error url scrap

--- index.js
+++ index.js
@@ -1,85 +1,65 @@
 const puppeteer = require("puppeteer");
-const config = require("./conf.json");
 const ProfileScrapper = require("./profileScrapper");
 async function timeout(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 linkArray = [];
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: false, devtools: true });
   const page = await browser.newPage();
-  await connect(page);
-  await accessSearchPage(page);
-  // await accessUserProfil(page)
-  // const profil = await retrieveProfil()
-  // console.log(profil)
-  await secondPage(page);
+  await page.setViewport({
+    width: 1200,
+    height: 800
+  });
   await scrapper(page);
   await browser.close();
 })();
-// connect directly to viadeo (need a page from puppeter)
-connect = async page => {
-  page.on("load", () => console.log("Loaded: " + page.url()));
-  await page.goto("https://fr.viadeo.com/fr/signin");
-  await page.type(
-    "#signin > div > main > form > div:nth-child(2) > input[type=text]",
-    config.email
-  );
-  await page.type(
-    "#signin > div > main > form > div:nth-child(3) > input[type=password]",
-    config.pass
-  );
-  await page.click("button");
-  await page.waitForNavigation();
-};
+
 accessSearchPage = async page => {
   //TODO
   name = "Arnaud";
 
-  await page.goto(`https://www.viadeo.com/fr/search/#/?q=${name}`);
-  await page.waitForSelector(
-    "#ember674 > div > div.gu.gu-last.gu-m-1of1.unified-search__profiles > div.bx.pbxs"
-  );
-  hrefs = await page.$$eval("h2 > a", as => as.map(a => a.href));
-  for (let href of hrefs) {
-    // open the page
-    try {
-      if (!href.includes("company")) {
-        linkArray.push(href);
-      }
-      // const profil = new ProfileScrapper(href, page)
-      // await profil.getProfile()
-    } catch (error) {
-      console.log(error);
-      console.log("failed to open the page: ", href);
-    }
-  }
-};
+  await page.goto(`https://www.qwant.com/?q=viadeo.com%20%2b${name}&t=web`);
+  await page.waitForSelector("div.result__url > span");
+  await autoScroll(page);
+  const sel = "div.result__url > span";
+  const langues = await page.evaluate(sel => {
+    let elements = Array.from(document.querySelectorAll(sel));
 
-secondPage = async page => {
-  await page.goto(`https://www.viadeo.com/fr/search/#/?page=2&q=${name}`);
-
-  await timeout(5000);
-
-  secondHrefs = await page.$$eval("h2 > a", as => as.map(a => a.href));
-  for (let href of secondHrefs) {
-    // open the page
-    try {
-      if (!href.includes("company")) {
-        linkArray.push(href);
-      }
-      // const profil = new ProfileScrapper(href, page)
-      // profil.getProfile()
-    } catch (error) {
-      console.log(error);
-      console.log("failed to open the page: ", href);
-    }
-  }
+    let links = elements.map(element => {
+      return element.textContent.includes("profile") ? element.textContent : 0;
+    });
+    return links;
+  }, sel);
+  return langues;
 };
 
 scrapper = async page => {
-  for (const link of linkArray) {
-    const profil = new ProfileScrapper(link, page);
-    await profil.getProfile();
+  s = await accessSearchPage(page);
+  for (const link of s) {
+    url = "https://" + link;
+    if (link !== 0) {
+      const profil = new ProfileScrapper(url, page);
+      await profil.getProfile();
+    }
   }
 };
+
+async function autoScroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      var totalHeight = 0;
+      var distance = 100;
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
